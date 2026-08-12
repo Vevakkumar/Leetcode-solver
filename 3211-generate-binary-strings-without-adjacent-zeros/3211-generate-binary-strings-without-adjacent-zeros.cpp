b8222cc4b07f87967531dfccdab3ae06fc29ea99
Vevakkumar <vevak9306@gmail.com>
2026-08-13 04:16:36 +0530

    Time: 7 ms (90.59%), Space: 20.8 MB (89.16%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    vector<string> ans;
 
-    void solve(int n, string &s) {
-        // If string length becomes n
-        if (s.length() == n) {
-            ans.push_back(s);
-            return;
-        }
-
-        // Choice 1: Put 1
-        s.push_back('1');
-        solve(n, s);
-        s.pop_back();
-
-        // Choice 2: Put 0
-        // Only allowed if previous character is not 0
-        if (s.empty() || s.back() != '0') {
-            s.push_back('0');
-            solve(n, s);
-            s.pop_back();
-        }
+void sub(string &s, vector<string> &ans,int n){
+    if(s.length()==n){
+        ans.push_back(s);
+        return;
     }
+    s.push_back('1');
+    sub(s,ans,n);
+    s.pop_back();
+
+
+    if(s.empty() || s.back()!='0'){
+        s.push_back('0');
+        sub(s,ans,n);
+        s.pop_back();
+    }
+}
 
     vector<string> validStrings(int n) {
         string s = "";
-        solve(n, s);
+        vector<string>ans;
+        sub(s,ans,n);
         return ans;
+
     }
 };
